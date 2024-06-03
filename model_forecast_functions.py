14bb82952228496a0eb5236052fed2a42ebea912
aminjafarlou <jafarlou.amin@gmail.com>
2024-06-03 17:55:25 +0330

    fix spelling

--- model_forecast_functions.py
+++ model_forecast_functions.py
@@ -157,7 +157,7 @@ test_get_surface_flow_rate_oil_and_gas()
 
 
 
-def get_sruface_flow_rate_water(bottom_hole_pressure, surface_pressure, bottom_hole_temperature, surface_temperature, bottom_hole_flow_rate_water):
+def get_surface_flow_rate_water(bottom_hole_pressure, surface_pressure, bottom_hole_temperature, surface_temperature, bottom_hole_flow_rate_water):
     all_initial_and_boundary_conditions = get_initial_and_boundary_conditions()
     compressibility_coefficient = all_initial_and_boundary_conditions["compressibility_coefficient"]
     thermal_expansion_coefficient = all_initial_and_boundary_conditions["thermal_expansion_coefficient"]
@@ -167,8 +167,8 @@ def get_sruface_flow_rate_water(bottom_hole_pressure, surface_pressure, bottom_h
     surface_flow_rate_water = bottom_hole_flow_rate_water_const_tmp * exp(thermal_expansion_coefficient * (surface_temperature - bottom_hole_temperature))
     return surface_flow_rate_water
 
-# Test get_sruface_flow_rage_water
-def test_get_sruface_flow_rate_water():
+# Test get_surface_flow_rage_water
+def test_get_surface_flow_rate_water():
     all_initial_and_boundary_conditions = get_initial_and_boundary_conditions()
     bottom_hole_pressure = all_initial_and_boundary_conditions["bottom_hole_pressure"]
     surface_pressure = all_initial_and_boundary_conditions["surface_pressure"]
@@ -176,18 +176,18 @@ def test_get_sruface_flow_rate_water():
     surface_temperature = all_initial_and_boundary_conditions["surface_temperature"]
     bottom_hole_flow_rate_water = all_initial_and_boundary_conditions["bottom_hole_flow_rate_water"]
 
-    tmp = get_sruface_flow_rate_water(bottom_hole_pressure = bottom_hole_pressure,
+    tmp = get_surface_flow_rate_water(bottom_hole_pressure = bottom_hole_pressure,
                                   surface_pressure = surface_pressure,
                                   bottom_hole_temperature = bottom_hole_temperature,
                                   surface_temperature = surface_temperature,
                                   bottom_hole_flow_rate_water = bottom_hole_flow_rate_water)
     assert abs(tmp - 0.04972476036029373) < 1e-5
-    print("get_sruface_flow_rate_water PASSED: ", tmp)
+    print("get_surface_flow_rate_water PASSED: ", tmp)
 
-test_get_sruface_flow_rate_water()
+test_get_surface_flow_rate_water()
 
 
 def model_forecast(bottom_hole_pressure, bottom_hole_temperature, surface_pressure, surface_temperature, bottom_hole_flow_rate_oil, bottom_hole_flow_rate_water, bottom_hole_flow_rate_gas):
-    # surface_flow_rate_water = get_sruface_flow_rage_water(bottom_hole_pressure, surface_pressure, bottom_hole_temperature, surface_temperature, bottom_hole_flow_rate_water)
+    # surface_flow_rate_water = get_surface_flow_rage_water(bottom_hole_pressure, surface_pressure, bottom_hole_temperature, surface_temperature, bottom_hole_flow_rate_water)
     pass
     # return surface_flow_rate_oil, surface_flow_rate_water, surface_flow_rate_gas
