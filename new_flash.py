9ec3312ed396a39bb4c714c1331963e9c15e3e3a
aminjafarlou <jafarlou.amin@gmail.com>
2024-06-07 10:43:49 +0330

    flash works correctly with different compositions and different pressure and temperature conditions

--- new_flash.py
+++ new_flash.py
@@ -2,8 +2,18 @@ from thermo import ChemicalConstantsPackage, CEOSGas, CEOSLiquid, PRMIX, FlashVL
 from thermo.interaction_parameters import IPDB
 
 components = [
-'methane','ethane','propane','butane','pentane','hexane','heptane','octane','nonane','decane',
+    'methane',
+    'ethane',
+    'propane',
+    'butane',
+    'pentane',
+    'hexane',
+    'heptane',
+    'octane',
+    'nonane',
+    'decane'
 ]
+composition_molar_fractions = [0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.15]
 
 constants, properties = ChemicalConstantsPackage.from_IDs(components)
 kijs = IPDB.get_ip_asymmetric_matrix('ChemSep PR', constants.CASs, 'kij')
@@ -12,10 +22,12 @@ eos_kwargs = {'Pcs': constants.Pcs, 'Tcs': constants.Tcs, 'omegas': constants.om
 gas = CEOSGas(PRMIX, eos_kwargs=eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases)
 liquid = CEOSLiquid(PRMIX, eos_kwargs=eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases)
 flasher = FlashVL(constants, properties, liquid=liquid, gas=gas)
-zs = [0.15, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05]
-PT = flasher.flash(T=300.0, P=1e5, zs=zs)
+PT = flasher.flash(T=400.0, P=2e6, zs=composition_molar_fractions)
 
-print("\n *******")
 print(PT.phase)
 print("\n *******")
+print(PT.VF)
 print("\n *******")
+print(PT.LF)
+print("\n *******")
+print(PT.LF + PT.VF)
